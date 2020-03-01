87a0b22f25679c74d02676b1b84cdd8b5fcf9b93
q1ncy <qbarnabas@gmail.com>
2020-03-01 03:49:02 +0300

    add js effects ie. display choice options

--- js/sign.js
+++ js/sign.js
@@ -1,15 +1,26 @@
-
-$("button").click(function(){
+$("button#signIn").click(function(){
     function signUp() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("psw").value;
     var details=[email , password ]
     console.log(details)
-    if (details=["", ""]){
+    if (details==["", ""]){
         alert("input details")
     }else{
         alert("welcome to pizza palace")
     }
     }
     signUp()
+})//correct
+$("button#entry").click(function(){
+    if (details==["", ""]){
+        alert("please sign up first")
+    }
+})//correct
+//display choice options
+$("button#clickOne").click(function(){
+    $(".card-one").fadeIn();
+})
+$("button#clickTwo").click(function(){
+    $(".card-two").fadeIn()
 })
